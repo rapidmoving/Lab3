ad3fa33ddf4a0c2c26b622984f64bd11dde181a4
Tasneem <thajudeentasneem@gmail.com>
2023-06-17 00:36:17 +0800

    Addition of average salary

--- employee_info.py
+++ employee_info.py
@@ -21,16 +21,15 @@ def get_employees_by_age_range(age_lower_limit, age_upper_limit):
 def calculate_average_salary():
     total = 0
     average = 0
-
-    #add your implementation to calculate here
-
+    for item in employee_data:
+        total += item["salary"]
+    average = total / (len(employee_data))
 
     return average
 
 def get_employees_by_dept(department):
     result = []
 
-    # Add your implementation from here
 
 
     return result
